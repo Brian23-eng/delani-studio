d7c1c41151a2d21f7db4e4450563a948027431e5
brian23-eng <b.odhiambo.bo@gmail.com>
2019-08-23 19:06:20 +0300

    add divs to images and the paragraphs in class 'what' so as to enble us to toggle the paragraphs in and out

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//         alert("this is  a header!");
+//     });
+// });
+
+
+
+
+$(document).ready(function() {
+    $("#hide-in").click(function() {
+        $("p").toggle();
+        $("p").toggle();
+    });
+});
